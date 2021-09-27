a9a05e0d468b7387f1b9ddb7d0dd6000ebd39749
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2021-09-28 01:38:16 +0500

    Update with test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -16,33 +16,7 @@
         <div class="card-body">
             <div id="qr-reader-results"></div>
             <table class="table" id="myTable">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
+                
             </table>
         </div>
     </div>
@@ -70,9 +44,9 @@
                     lastResult = decodedText;
                     // Handle on success condition with the decoded message.
                     // console.log(`Scan result ${decodedText}`, decodedResult);
-                    alert(decodedText);
-                    console.log(decodedText);
-                    console.log(decodedResult);
+                    // alert(decodedText);
+                    // console.log(decodedText);
+                    // console.log(decodedResult);
                     // Find a <table> element with id="myTable":
                     var table = document.getElementById("myTable");
 
@@ -81,11 +55,11 @@
 
                     // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
                     var cell1 = row.insertCell(0);
-                    var cell2 = row.insertCell(1);
+                    // var cell2 = row.insertCell(1);
 
                     // Add some text to the new cells:
-                    cell1.innerHTML = "NEW CELL1";
-                    cell2.innerHTML = "NEW CELL2";
+                    cell1.innerHTML = decodedText;
+                    // cell2.innerHTML = "NEW CELL2";
 
                 }
             }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,6 +76,7 @@
                             <a href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
+            <a href="/test">Scan barcode</a>
                 </div>
             @endif
 
