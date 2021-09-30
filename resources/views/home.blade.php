321976bc24d3c509bcc40a7467dec6fa667d8700
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2021-10-01 00:14:12 +0500

    Push update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,6 +43,54 @@
 
 
 <script src="">
+           function docReady(fn) {
+            // see if DOM is already available
+            if (document.readyState === "complete" ||
+                document.readyState === "interactive") {
+                // call on next available tick
+                setTimeout(fn, 1);
+            } else {
+                document.addEventListener("DOMContentLoaded", fn);
+            }
+        }
 
+        docReady(function() {
+            var resultContainer = document.getElementById('qr-reader-results');
+            var lastResult, countResults = 0;
+
+            function onScanSuccess(decodedText, decodedResult) {
+                if (decodedText !== lastResult) {
+                    ++countResults;
+                    lastResult = decodedText;
+                    // Handle on success condition with the decoded message.
+                    // console.log(`Scan result ${decodedText}`, decodedResult);
+                    // alert(decodedText);
+                    // console.log(decodedText);
+                    // console.log(decodedResult);
+                    // Find a <table> element with id="myTable":
+                    var table = document.getElementById("myTable");
+
+                    // // Create an empty <tr> element and add it to the 1st position of the table:
+                    var row = table.insertRow(0);
+
+                    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+                    var cell1 = row.insertCell(0);
+                    // var cell2 = row.insertCell(1);
+
+                    // Add some text to the new cells:
+                    cell1.innerHTML = decodedText;
+                    // cell2.innerHTML = "NEW CELL2";
+                    $('#qr-reader-results').append('<input type="hidden" value="'+decodedText+'"" name="value[]"/>');
+                    $('#btn-submit').show();
+                }
+            }
+
+            var html5QrcodeScanner = new Html5QrcodeScanner(
+                "qr-reader", {
+                    fps: 15,
+                    qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+                }, true);
+            html5QrcodeScanner.render(onScanSuccess);
+        });
 </script>
 @endsection
