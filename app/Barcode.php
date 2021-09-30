2ef8db42c6f4d359abbf06619a16938f05a4f20e
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2021-10-01 00:33:19 +0500

    Update has been uploaded

--- app/Barcode.php
+++ app/Barcode.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barcode extends Model
 {
-    protected $filable = [
+    protected $fillable = [
         'id',
         'barcode_value',
         'created_at',
